e948f5761bb137832d1fb87202981bf71121c038
Fernando Castillo <superferenabled@gmail.com>
2025-12-05 23:40:12 -0600

    added navigation to the gender page, and added buttons to navigate to

--- src/shop/components/CustomHeader.tsx
+++ src/shop/components/CustomHeader.tsx
@@ -1,13 +1,15 @@
 import {Button} from "@/components/ui/button.tsx";
 import {Input} from "@/components/ui/input.tsx";
-import {Menu, Search, ShoppingBag} from "lucide-react";
-import {useRef, useState} from "react";
-import {useSearchParams} from "react-router";
+import {Menu, Search} from "lucide-react";
+import {useRef} from "react";
+import {Link, useParams, useSearchParams} from "react-router";
+import {cn} from "@/lib/utils.ts";
 
 export const CustomHeader = () => {
-    const [cartCount] = useState(3);
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const {gender} = useParams();
+    console.log({gender});
 
     const inputRef = useRef<HTMLInputElement>(null);
     const query = searchParams.get('query') || '';
@@ -41,18 +43,23 @@ export const CustomHeader = () => {
 
                     {/* Navigation - Desktop */}
                     <nav className="hidden md:flex items-center space-x-8">
-                        <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                            Camisetas
-                        </a>
-                        <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                            Sudaderas
-                        </a>
-                        <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                            Chaquetas
-                        </a>
-                        <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                            Accesorios
-                        </a>
+                        <Link to="/" className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                            !gender ? 'underline underline-offset-4' : '')}>
+                            Todos
+                        </Link>
+                        <Link to="/gender/men" className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                            gender === 'men' ? 'underline underline-offset-4' : '')}>
+                            Hombres
+                        </Link>
+                        <Link to="/gender/woman"
+                              className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                                  gender === 'woman' ? 'underline underline-offset-4' : '')}>
+                            Mujeres
+                        </Link>
+                        <Link to="/gender/kid" className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                            gender === 'kid' ? 'underline underline-offset-4' : '')}>
+                            Niños
+                        </Link>
                     </nav>
 
                     {/* Search and Cart */}
@@ -75,13 +82,18 @@ export const CustomHeader = () => {
                             <Search className="h-5 w-5"/>
                         </Button>
 
-                        <Button variant="ghost" size="icon" className="relative">
-                            <ShoppingBag className="h-5 w-5"/>
-                            {cartCount > 0 && <span
-                                className="absolute -top-1 -right-1 h-5 w-5 rounded-full bg-primary text-primary-foreground text-xs flex items-center justify-center">
-                  {cartCount}
-                </span>}
-                        </Button>
+                        <Link to="/auth/login">
+                            <Button variant="default" size="sm" className="ml-2">
+                                Login
+                            </Button>
+                        </Link>
+
+                        <Link to="/admin">
+                            <Button variant="destructive" size="sm" className="ml-2">
+                                Admin
+                            </Button>
+                        </Link>
+
                     </div>
                 </div>
             </div>
--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,8 +1,18 @@
+import {CustomJumbotron} from "@/shop/components/CustomJumbotron.tsx";
+import {ProductsGrid} from "@/shop/components/ProductsGrid.tsx";
+import {products} from "@/mocks/products.mock.ts";
+import {CustomPagination} from "@/components/custom/CustomPagination.tsx";
+import {useParams} from "react-router";
 
 export const GenderPage = () => {
+
+    const {gender} = useParams();
+    const genderLabel = gender === "woman" ? "Mujer" : (gender === "men" ? "Hombre" : "Niños");
     return (
-        <div>
-            <h1>Gender Page</h1>
-        </div>
+        <>
+            <CustomJumbotron title={`Productos para ${genderLabel}`} />
+            <ProductsGrid products={products} />
+            <CustomPagination totalPages={5} />
+        </>
     );
 };
